b5475e83d76b671d0abb9bd8b4718a5f8fda73a2
Taco de Wolff <tacodewolff@gmail.com>
2024-05-02 09:48:31 -0400

    Update to v2.20.20

--- Formula/minify.rb
+++ Formula/minify.rb
@@ -1,8 +1,8 @@
 class Minify < Formula
   desc "Minifier CLI for HTML, CSS, JS, JSON, SVG and XML"
   homepage "https://github.com/tdewolff/minify"
-  url "https://github.com/tdewolff/minify/archive/v2.20.19.tar.gz"
-  sha256 "705356c3d7eb2e773557a280579c1dcbcda5c78378ea77dd346f7a367946f5e1"
+  url "https://github.com/tdewolff/minify/archive/v2.20.20.tar.gz"
+  sha256 "15027db4d856e72482e0ff7b3cee8bcb9e3dcee44ef3e4462de9c11f4e6066b6"
 
   depends_on "go" => :build
 
